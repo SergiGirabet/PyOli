d083b4adbcd7dfb48628451e35cacfa284030895
jaumeguasch <jguaschllobera@gmail.com>
2021-03-29 02:04:09 +0200

    Implemented models.py

--- booking_manager/admin.py
+++ booking_manager/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Booking
+from .models import Client, Order, Table, Booking, Category, Product, ProductOrder
+
 # Register your models here.
 
+admin.site.register(Client)
+admin.site.register(Order)
+admin.site.register(Table)
 admin.site.register(Booking)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductOrder)
--- booking_manager/models.py
+++ booking_manager/models.py
@@ -7,25 +7,64 @@ from address.models import AddressField
 class Client(models.Model):
     client = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{type(self).__name__}(id={self.id}, username={self.client.username})"
+
+
 class Order(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    address = AddressField(on_delete=models.CASCADE, null=False, blank=False)
-    date = models.DateTimeField()
+    address = AddressField(on_delete=models.CASCADE)  # default for null & blank is false
+    date_order = models.DateTimeField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{type(self).__name__}(id={self.id}, client={self.client})"
+
+
+class Table(models.Model):
+    reserve = models.BooleanField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Booking(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
     date = models.DateTimeField()
-    name = models.CharField(max_length=30, null=True, blank=True)
+
+    people_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{type(self).__name__}(table={self.table}, date={self.date.strftime('%d/%m/%Y - %H:%M')})"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, unique=True)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    stock = models.IntegerField()
+    modifiable = models.BooleanField()
 
     def __str__(self):
         return self.name
 
 
-class Table(models.Model):
-    capacity = models.IntegerField(null=True, blank=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class ProductOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
     def __str__(self):
-        return self.id
-
+        return f"{type(self).__name__}(product={self.product}, order={self.order}, quantity={self.quantity})"
