e41c676a57a4564c13c8e4262555e928b5836095
Alex Florensa <florensa14@gmail.com>
2021-04-10 17:34:39 +0200

    fixed bad import

--- booking_manager/admin.py
+++ booking_manager/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Client, Order, Table, Booking, Category, Product, ProductOrder
+from .models import Address, Order, Table, Booking, Category, Product, ProductOrder
 
 # Register your models here.
 
-admin.site.register(Client)
+admin.site.register(Address)
 admin.site.register(Order)
 admin.site.register(Table)
 admin.site.register(Booking)
