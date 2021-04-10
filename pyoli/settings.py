db18180fcbe74270fb92998b75c205d0684be5c3
Alex Florensa <49005351+alexflorensa@users.noreply.github.com>
2021-04-10 12:45:07 +0200

    Update settings.py

--- pyoli/settings.py
+++ pyoli/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '+284d@oqv0bo8&02$a#x4g(%&rtfdb2recv+^!7r#)5y93mar)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
