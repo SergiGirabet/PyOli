d0a1e8223bf0b21617f6c1af50e06b52f875509c
Alex Florensa <49005351+alexflorensa@users.noreply.github.com>
2021-04-10 12:42:56 +0200

    Update settings.py

--- pyoli/settings.py
+++ pyoli/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+284d@oqv0bo8&02$a#x4g(%&rtfdb2recv+^!7r#)5y93mar)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
