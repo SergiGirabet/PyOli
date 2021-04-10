a0a57c9cce1747e30c86c34b60522f7375214479
Alex Florensa <florensa14@gmail.com>
2021-04-10 18:21:55 +0200

    updated models

--- booking_manager/models.py
+++ booking_manager/models.py
@@ -10,13 +10,13 @@ class Address(models.Model):
 
 
 class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    order_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    deliver_address = models.ForeignKey(Address, on_delete=models.CASCADE)
     date_order = models.DateTimeField()
     date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{type(self).__name__}(id={self.id}, user={self.user})"
+        return f"{type(self).__name__}(id={self.id}, user={self.order_user})"
 
 
 class Table(models.Model):
@@ -28,14 +28,14 @@ class Table(models.Model):
 
 
 class Booking(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    booking_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reserved_table = models.ForeignKey(Table, on_delete=models.CASCADE)
     date = models.DateTimeField()
 
     people_number = models.IntegerField()
 
     def __str__(self):
-        return f"{type(self).__name__}(table={self.table}, date={self.date.strftime('%d/%m/%Y - %H:%M')})"
+        return f"{type(self).__name__}(table={self.reserved_table}, date={self.date.strftime('%d/%m/%Y - %H:%M')})"
 
 
 class Category(models.Model):
@@ -49,7 +49,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=50, unique=True)
     price = models.DecimalField(decimal_places=2, max_digits=8)
     stock = models.IntegerField()
@@ -60,9 +60,9 @@ class Product(models.Model):
 
 
 class ProductOrder(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    ordered_product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    related_order = models.ForeignKey(Order, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
     def __str__(self):
-        return f"{type(self).__name__}(product={self.product}, order={self.order}, quantity={self.quantity})"
+        return f"{type(self).__name__}(product={self.ordered_product}, order={self.related_order}, quantity={self.quantity})"
